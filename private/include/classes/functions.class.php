2b7f43c06dc33b818a21a43d7fac241c0d52ee80
Engin Yapici <enginyapici@gmail.com>
2021-02-08 19:11:49 -0800

    Remove mcrypt methods and simplify encode and decode

--- private/include/classes/functions.class.php
+++ private/include/classes/functions.class.php
@@ -3,8 +3,6 @@ class Functions {
     /* ################################### -- Encryption Functions -- ##################################### */
     /* #################################################################################################### */
 
-    var $skey = "dvds#4fw00#wfkm!8jwd#&mhg%uy34wb";
-
     public function safe_b64encode($string) {
         $data = base64_encode($string);
         $data = str_replace(array('+', '/', '='), array('-', '_', ''), $data);
@@ -24,22 +22,14 @@ class Functions {
         if (!$value) {
             return false;
         }
-        $text = $value;
-        $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB);
-        $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
-        $crypttext = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $this->skey, $text, MCRYPT_MODE_ECB, $iv);
-        return trim($this->safe_b64encode($crypttext));
+        return $this->safe_b64encode($value);
     }
 
     public function decode($value) {
         if (!$value) {
             return false;
         }
-        $crypttext = $this->safe_b64decode($value);
-        $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB);
-        $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
-        $decrypttext = mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $this->skey, $crypttext, MCRYPT_MODE_ECB, $iv);
-        return trim($decrypttext);
+        return $this->safe_b64decode($value);
     }
 
     /* /  ************************************************************************************************* */
