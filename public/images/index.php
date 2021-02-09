693eb3cefb670e7e66fa964c5e2e05cd165ef3e3
Engin Yapici <enginyapici@gmail.com>
2021-02-08 18:42:23 -0800

    Remove index.php copyright comments

--- public/images/index.php
+++ public/images/index.php
@@ -1,11 +1,4 @@
 <?php
-/* ================================================================ */
-/* Created by Engin Yapici on 11/03/2014                            */
-/* Last modified by Engin Yapici on 10/14/2015                      */
-/* Copyright Engin Yapici, 2015.                                    */
-/* enginyapici@gmail.com                                            */
-/* ================================================================ */
-
 // Redirecting to the homepage
 header("Location: /");
 exit;
